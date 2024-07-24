017ec2c07cf81216322ec52dc916093794381860
dannybarret <74153177+dannybarret@users.noreply.github.com>
2024-07-23 21:32:39 -0500

    Add files via upload

---
+++ nltk_utils.py
@@ -0,0 +1,38 @@
+import numpy as np
+import nltk
+# nltk.download('punkt')
+from nltk.stem import SnowballStemmer
+stemmer = SnowballStemmer('spanish')
+
+def tokenize(sentence):
+    """
+    split sentence into array of words/tokens
+    a token can be a word or punctuation character, or number
+    """
+    return nltk.word_tokenize(sentence, language='spanish')
+
+def stem(word):
+    """
+    stemming = find the root form of the word
+    examples:
+    words = ["organize", "organizes", "organizing"]
+    words = [stem(w) for w in words]
+    -> ["organ", "organ", "organ"]
+    """
+    return stemmer.stem(word.lower())
+
+def bag_of_words(tokenized_sentence, words):
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hola", "como", "son", "tu"]
+    words = ["alo", "hola", "yo", "tu", "chao", "gracias", "bien"]
+    bog   = [  0  ,     1 ,   0 ,   1 ,     0 ,        0 ,      0]
+    """
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words:
+            bag[idx] = 1
+    return bag
